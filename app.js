b58155a72b1c72150d8a10893cf9aab956037624
Daniele-SS <dev.daniele@outlook.com>
2026-05-05 08:20:44 -0300

    feat: adiciona a URL junto as informações da function que irá retornar os dados da API

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict'
+
+//Função assíncrona para retornar as noticias
+async function getDadosNoticias() {
+    const url       = `https://gnews.io/api/v4/{endpoint}?{parameters}&apikey=1bf7720515e16b9161bdbfed9f6641a2`
+    const response  = await fetch(url)
+    const data      = await response.json()
+
+    return data 
+}
