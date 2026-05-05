f2aaa1facb682d1468a648012f2f93e9bf25b5a9
Daniele-SS <dev.daniele@outlook.com>
2026-05-05 11:24:37 -0300

    feat: adiciona a URL

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 //Função assíncrona para retornar as noticias
 async function getDadosNoticias() {
-    const url       = `https://gnews.io/api/v4/{endpoint}?{parameters}&apikey=1bf7720515e16b9161bdbfed9f6641a2`
+    const url       = `https://gnews.io/api/v4/top-headlines?category&lang=pt&country=br&max=10&apikey=1bf7720515e16b9161bdbfed9f6641a2`
     const response  = await fetch(url)
     const data      = await response.json()
 
